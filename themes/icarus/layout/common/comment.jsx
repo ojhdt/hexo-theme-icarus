3470734b1107254a837e071a70d239e5aa63f308
ojhdt <ojhdtmail@gmail.com>
2021-06-05 22:48:09 +0800

    comment enabled

--- themes/icarus/layout/common/comment.jsx
+++ themes/icarus/layout/common/comment.jsx
@@ -20,7 +20,8 @@ module.exports = class extends Component {
                         Comment = Comment.Cacheable ? Comment.Cacheable : Comment;
                         return <Comment config={config} page={page} helper={helper} comment={comment} />;
                     } catch (e) {
-                        logger.w(`Icarus cannot load comment "${comment.type}"`);
+                        // logger.w(`Icarus cannot load comment "${comment.type}"`);
+                        logger.w(e);
                         return null;
                     }
                 })()}
